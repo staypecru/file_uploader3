400ac34d714aef923f22c39cc7389a7dd9d65461
syakovlev <syakovlev@moysklad.ru>
2025-06-17 15:33:11 +0700

    change stimulus do direct upload

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { start } from "@rails/activestorage"
+start()
+import "controllers/upload_progress_controller"
--- app/javascript/controllers/upload_progress_controller.js
+++ app/javascript/controllers/upload_progress_controller.js
@@ -1,38 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+document.addEventListener("direct-upload:initialize", (event) => {
+  const { detail } = event
+  const { file } = detail
+  if (!file) return
 
-export default class extends Controller {
-  static targets = ["progress"]
+  const progress = document.getElementById("upload_progress")
+  progress.value = 0
+})
 
-  connect() {
-    this.element.addEventListener("submit", this.startUpload.bind(this))
+document.addEventListener("direct-upload:progress", (event) => {
+  if (event.lengthComputable) {
+    const percent = Math.round((event.loaded / event.total) * 100)
+    progress.value = percent
   }
-
-  startUpload(event) {
-    const form = this.element
-    const fileInput = form.querySelector("input[type=file]")
-    const file = fileInput.files[0]
-    if (!file) return
-
-    const progress = this.progressTarget
-    const xhr = new XMLHttpRequest()
-    const formData = new FormData(form)
-
-    xhr.upload.addEventListener("progress", (e) => {
-      if (e.lengthComputable) {
-        const percent = Math.round((e.loaded / e.total) * 100)
-        progress.value = percent
-      }
-    })
-
-    xhr.onload = () => {
-      progress.value = 100
-      Turbo.visit("/")
-    }
-
-    xhr.open(form.method, form.action)
-    xhr.setRequestHeader("Accept", "text/vnd.turbo-stream.html")
-    xhr.send(formData)
-
-    event.preventDefault()
-  }
-}
+})
